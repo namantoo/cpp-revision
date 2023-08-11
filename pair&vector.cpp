253200cfbc08b53e7f8fd1668294be774ea12e8f
Naman <namanmail4@gmail.com>
2023-08-11 23:39:03 +0530

    vectors

---
+++ pair&vector.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+void explainPair(){
+    pair<int, int> p = {1,3};
+    
+    cout<<p.first<<" "<<p.second<<endl;
+    
+    pair<int, pair<int,int>> q = {1,{4,5}};
+    
+    cout<<q.first<<" "<<q.second.first<<endl;
+    
+    pair<int, int> arr[] = { {1,2} , {4,5}, {6,7}};
+    
+    cout<<arr[1].second<<" should be 5"<<endl;
+}
+int main() {
+    // Write C++ code here
+    cout << "Hello world!"<<endl;
+    explainPair()
+    return 0;
+}
+
+ 
